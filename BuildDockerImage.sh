a513172453b340c5fbf0d75c61d86773624ed39e
Ilan Bowen <ilanbowen@gmail.com>
2019-06-16 00:43:38 +0300

    Splitting Servers 22

--- BuildDockerImage.sh
+++ BuildDockerImage.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd /home/ubuntu/midproject/Docker
-sudo docker build -t ilanbowen/myflaskproj-img:latest .
+sudo docker build -f /home/ubuntu/midproject/Docker/Dockerfile -t ilanbowen/myflaskproj-img:latest .
