ff9b1ed746784e6a870da1a022604d92f7726869
Ilan Bowen <ilanbowen@gmail.com>
2019-06-15 20:09:39 +0300

    Splitting Servers 13

---
+++ BuildDockerImage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/midproject/Docker
+sudo docker build -t ilanbowen/myflaskproj-img:latest .
---
+++ DeployDockerAppInK8s.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/ubuntu/midproject/Kubernetes/createpodandservice.sh
