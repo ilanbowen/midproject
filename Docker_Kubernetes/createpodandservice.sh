d3cfc6f65ff1b36000a760f18a58891601d500be
Ilan Bowen <ilanbowen@gmail.com>
2019-07-02 12:18:06 +0300

    After Consul 23

--- Docker_Kubernetes/createpodandservice.sh
+++ Docker_Kubernetes/createpodandservice.sh
@@ -1,2 +1,2 @@
-sudo kubectl create -f /home/ubuntu/midproject/Docker_Kubernetes/flask_pod.yml
-#sudo kubectl create -f /home/ubuntu/midproject/Docker_Kubernetes/flask_rs.yml
+#sudo kubectl create -f /home/ubuntu/midproject/Docker_Kubernetes/flask_pod.yml
+sudo kubectl create -f /home/ubuntu/midproject/Docker_Kubernetes/flask_rs.yml
